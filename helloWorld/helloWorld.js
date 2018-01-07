25b35181469ae605f1ea84d174702173f573eb92
maggiewiseman <maggie.wiseman@gmail.com>
2018-01-07 16:54:25 -0500

    added working tsconfig for hellowworld

--- helloWorld/helloWorld.js
+++ helloWorld/helloWorld.js
@@ -1,5 +1,7 @@
 var HellowWorld = /** @class */ (function () {
     function HellowWorld() {
+        this.helloWorld = "hello";
     }
     return HellowWorld;
 }());
+//# sourceMappingURL=helloWorld.js.map
